3777089a179b2d279d794ed81fa8974c99068192
Yarick Levchenco <jaroslav.levchenco11@gmail.com>
2022-06-05 12:34:43 +0300

    Header wrapper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import Header from './components/Header/header';
+import AppContext from './context/AppContext';
+import './App.css'
 
 function App() {
+  const [state, setState] = useState({
+    isAuth: false,
+  })
+  
   return (
     <div>
-      <Header />
+      <AppContext.Provider value = {{state}} >
+        <Header />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,6 +1,24 @@
+import './header.css'
+import authImg from '../../img/user-in.png'
+import noAuthImg from '../../img/user-out.png'
+import { useContext } from 'react'
+import AppContext from '../../context/AppContext'
+
 const Header = () => {
+    const {state} = useContext(AppContext)
+
     return(
         <header className='header'>
+            <div className="header_container _container">
+                <div className="header_body">
+                    <h1 className="header-logo">Messenger</h1>
+                    <div className="header-auth">
+                        {!state.isAuth && <span className='header-auth_text'>You aren't in your account</span>}
+                        {state.isAuth && <span className='header-auth_text'>Login</span>}
+                        <img src={!state.isAuth ? noAuthImg : authImg} alt={noAuthImg} />
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
